c9d035b8f500ab5ad40491e279997e90a5bb923f
Mustafa Gönül <mustafagonul@gmail.com>
2014-09-12 18:11:25 +0300

    Initial commit

---
+++ translations/organizer_tr.ts
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>Goods</name>
+    <message>
+        <location filename="../goods.ui" line="27"/>
+        <source>Goods</source>
+        <translation>Ürün</translation>
+    </message>
+    <message>
+        <location filename="../goods.ui" line="87"/>
+        <source>Exit</source>
+        <translation>Çıkış</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="139"/>
+        <source>Company Name</source>
+        <translation>Şirket İsmi</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="140"/>
+        <source>City</source>
+        <translation>Şehir</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="141"/>
+        <source>Name</source>
+        <translation>İsim</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="142"/>
+        <source>Tel</source>
+        <translation>Tel</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="143"/>
+        <source>Mobil</source>
+        <translation>Cep</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="144"/>
+        <source>Address</source>
+        <translation>Adres</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="145"/>
+        <source>E-Mail</source>
+        <translation>E-Posta</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="146"/>
+        <source>Taxation No</source>
+        <translation>Vergi No</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="19"/>
+        <source>Organizer</source>
+        <translation>Elektronik Ajanda</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="31"/>
+        <source>Show</source>
+        <translation>Göster</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="38"/>
+        <source>Goods</source>
+        <translation>Ürün</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="45"/>
+        <source>Add</source>
+        <translation>Ekle</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="52"/>
+        <source>Delete</source>
+        <translation>Sil</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Find</source>
+        <translation>Bul</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="66"/>
+        <source>Edit</source>
+        <translation>Düzenle</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="73"/>
+        <source>Show All</source>
+        <translation>Hepsini Göster</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="80"/>
+        <source>Exit</source>
+        <translation>Çıkış</translation>
+    </message>
+</context>
+<context>
+    <name>Purchaser</name>
+    <message>
+        <location filename="../purchaser.ui" line="74"/>
+        <source>Exit</source>
+        <translation>Çıkış</translation>
+    </message>
+    <message>
+        <location filename="../purchaser.ui" line="126"/>
+        <source>COMPANY</source>
+        <translation>ŞİRKET</translation>
+    </message>
+    <message>
+        <location filename="../purchaser.ui" line="148"/>
+        <source>Name</source>
+        <translation>İsim</translation>
+    </message>
+    <message>
+        <location filename="../purchaser.ui" line="193"/>
+        <source>CITY</source>
+        <translation>ŞEHİR</translation>
+    </message>
+    <message>
+        <location filename="../purchaser.ui" line="215"/>
+        <source>Mobil</source>
+        <translation>Cep</translation>
+    </message>
+    <message>
+        <location filename="../purchaser.ui" line="237"/>
+        <source>Tel</source>
+        <translation>Tel</translation>
+    </message>
+    <message>
+        <location filename="../purchaser.ui" line="259"/>
+        <source>Tax No</source>
+        <translation>Vergi No</translation>
+    </message>
+    <message>
+        <location filename="../purchaser.ui" line="281"/>
+        <source>E-mail</source>
+        <translation>E-posta</translation>
+    </message>
+    <message>
+        <location filename="../purchaser.ui" line="303"/>
+        <source>Adress</source>
+        <translation>Adres</translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="23"/>
+        <location filename="../mainwindow.cpp" line="183"/>
+        <source>Show</source>
+        <translation>Göster</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="24"/>
+        <source>Goods</source>
+        <translation>Ürün</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="73"/>
+        <source>All</source>
+        <translation>Hepsi</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="183"/>
+        <location filename="../mainwindow.cpp" line="231"/>
+        <location filename="../mainwindow.cpp" line="265"/>
+        <location filename="../mainwindow.cpp" line="282"/>
+        <source>You must select a row</source>
+        <translation>Bir satır seçmelisiniz</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="231"/>
+        <location filename="../mainwindow.cpp" line="236"/>
+        <source>Delete</source>
+        <translation>Sil</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="237"/>
+        <source>Are you sure to
+delete this record?</source>
+        <translation>Bu kayıdı silmek
+istediğinizden emin misiniz?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="265"/>
+        <location filename="../mainwindow.cpp" line="282"/>
+        <source>Edit</source>
+        <translation>Düzenle</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../connection.h" line="20"/>
+        <source>Cannot open database</source>
+        <translation>Veritabanı açılamıyor</translation>
+    </message>
+    <message>
+        <location filename="../connection.h" line="21"/>
+        <source>Unable to establish a database connection.
+Click Cancel to exit.</source>
+        <translation>Veritabanı bağlantısı kurulamıyor.
+Çıkmak için Çıkış&apos;a basınız.</translation>
+    </message>
+</context>
+</TS>
